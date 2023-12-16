4c3b066cefde4fe87289eeb19a32dbe8bd427ac9
using0601 <t555555561@gmail.com>
2023-12-17 01:34:55 +0800

    add something

---
+++ generate_pointcloud/json2ply.py
@@ -0,0 +1,39 @@
+import os
+import json
+import numpy as np
+import open3d as o3d
+from tqdm import tqdm
+
+
+# data = 'output/ZJU_comap/vertices/000001.json'
+# f = open(data)
+
+# data = json.load(f)
+# xyz = np.array(data[0]["vertices"])
+# pcd = o3d.geometry.PointCloud()
+# pcd.points = o3d.utility.Vector3dVector(xyz)
+
+# o3d.io.write_point_cloud("pcd/ZJU/000001.ply", pcd)
+
+def main(dataset_name):
+    path = os.path.join("output", dataset_name, "vertices")
+    os.makedirs(os.path.join("pcd", dataset_name), exist_ok = True)
+
+    files = os.listdir(path)
+    for filename in files:
+        file_path = os.path.join(path, filename)
+        f = open(file_path)
+        data = json.load(f)
+        xyz = np.array(data[0]["vertices"])
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(xyz)
+
+        o3d.io.write_point_cloud(os.path.join("pcd", dataset_name, filename.replace('.json', '.ply')), pcd)
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dataset_name', type=str)
+    args = parser.parse_args()
+    main(args.dataset_name)
